f8ae15fceefc4cc0622e04941349a8038436663b
miyado <hmiyado@gmail.com>
2020-02-25 01:05:46 +0900

    compare date in local time zone

--- lib/daily_report_generator/report_source.rb
+++ lib/daily_report_generator/report_source.rb
@@ -2,6 +2,7 @@
 
 require 'time'
 require 'octokit'
+require 'pry'
 require 'daily_report_generator/github/events'
 require 'daily_report_generator/github/event_adapter'
 
@@ -17,7 +18,10 @@ module DailyReportGenerator
       def today
         now = Time.now
         github_events = github_events()
-        github_events.filter { |event| event.created_at.year == now.year && event.created_at.month == now.month && event.created_at.day == now.day }
+        github_events.filter do |event|
+          created_at_local = event.created_at.getlocal
+          created_at_local.year == now.year && created_at_local.month == now.month && created_at_local.day == now.day
+        end
       end
     end
   end
