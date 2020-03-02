169c40256271a0ddcf0cdcb9df60e9b1e684eec1
miyado <hmiyado@gmail.com>
2020-03-02 16:35:17 +0900

    add google calendar event adapter

---
+++ lib/daily_report_generator/google_calendar/event_adapter.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'daily_report_generator/report_event'
+require 'time'
+
+module DailyReportGenerator
+  module GoogleCalendar
+    module EventAdapter
+      class << self
+        # @param events [Array<Google::Apis::CalendarV3::Event>]
+        # @return [Array<DailyReportGenerator::ReportEvent>]
+        def from(events)
+          events.map! { |event| from_event(event) }.compact
+        end
+
+        private
+
+        # @param event [Google::Apis::CalendarV3::Event]
+        # @return [DailyReportGenerator::ReportEvent]
+        def from_event(event)
+          source = 'google calendar'
+          event_type = 'MTG'
+          created_at = Time.parse(event.start.date_time.to_s)
+
+          # [Google::Apis::CalendarV3::Event::Source]
+          event_source = event.source
+
+          url = event_source.url
+          summary = event_source.title
+          detail = ''
+          DailyReportGenerator::ReportEvent.new(source, event_type, created_at, url, summary, detail)
+        end
+      end
+    end
+  end
+end
---
+++ spec/lib/google_calendar/event_adapter_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+require 'daily_report_generator/google_calendar/event_adapter'
+require 'ostruct'
+require 'json'
+
+RSpec.describe 'GoogleCalendar::EventAdapter' do
+  it 'should convert google calendar events' do
+    original_event_hash = {
+      start: {
+        date_time: '2020-01-01T12:34+0900'
+      },
+      source: {
+        title: 'google calendar event title',
+        url: 'https://event.url'
+      }
+    }
+    original_event = JSON.parse(original_event_hash.to_json, object_class: OpenStruct)
+    original_event.start.date_time = DateTime.parse(original_event.start.date_time)
+
+    actual_events = DailyReportGenerator::GoogleCalendar::EventAdapter.from([original_event])
+    actual_event = actual_events[0]
+
+    expect(actual_event.source).to eq 'google calendar'
+    expect(actual_event.event_type).to eq 'MTG'
+    expect(actual_event.created_at).to eq Time.parse('2020-01-01T12:34:00+09:00')
+    expect(actual_event.url).to eq 'https://event.url'
+    expect(actual_event.summary).to eq 'google calendar event title'
+    expect(actual_event.detail).to eq ''
+  end
+end
