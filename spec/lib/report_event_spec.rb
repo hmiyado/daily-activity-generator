fce29ce1ba683582646b05296f8f64b86b50fb9a
miyado <hmiyado@gmail.com>
2020-03-02 13:05:15 +0900

    fix test data

--- spec/lib/report_event_spec.rb
+++ spec/lib/report_event_spec.rb
@@ -7,7 +7,7 @@ require 'daily_report_generator/report_event'
 RSpec.describe 'ReportEvent' do
   it 'should return oneline log' do
     event = DailyReportGenerator::ReportEvent.new(
-      'https://source.com',
+      'source',
       'eventtype',
       Time.parse('2000-01-01T12:34:56+09:00'),
       'https://url.com',
