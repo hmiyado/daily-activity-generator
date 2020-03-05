36889bca6ca49d49e98938115f01aa4ea6adf80d
takuma-yoshida <takuma_yoshida@dwango.co.jp>
2020-03-05 18:39:34 +0900

    add from/to options and enable to specify time range

--- lib/roko/source/configurable.rb
+++ lib/roko/source/configurable.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+require 'date'
+require 'time'
+
 module Roko
   module Source
     # @!attribute start
@@ -9,19 +12,24 @@ module Roko
     module Configurable
       attr_accessor :start, :end
 
-      def today
-        @start = Date.today.to_time
-        @end = Date.today.next.to_time
-      end
-
-      def setup
-        today
+      def setup(options)
+        today = Date.today
+        @start = parse_time_or_nil(options[:from]) || today.to_time
+        @end = parse_time_or_nil(options[:to]) || today.next.to_time
       end
 
       # @param configurable [Roko::Source::Configurable]
       def configure_with(configurable)
         @start = configurable.start
         @end = configurable.end
+      end
+
+      private
+
+      def parse_time_or_nil(str)
+        Time.parse(str)
+      rescue ArgumentError
+        nil
       end
     end
   end
