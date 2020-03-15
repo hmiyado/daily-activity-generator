d962b3cd2918b2a70e7243955731b8d5fa8d2aef
miyado <hmiyado@gmail.com>
2020-03-15 14:51:15 +0900

    ignore line separators( replace to space)

--- spec/lib/report_event_spec.rb
+++ spec/lib/report_event_spec.rb
@@ -14,9 +14,24 @@ RSpec.describe 'ReportEvent' do
       'summary',
       'detail'
     )
-    
+
     oneline = event.oneline
 
-    expect(oneline).to eq '2000/01/01 12:34 eventtype [summary](https://url.com)' 
+    expect(oneline).to eq '2000/01/01 12:34 eventtype [summary](https://url.com)'
+  end
+
+  it 'should ignore line separators when oneline log' do
+    event = Roko::ReportEvent.new(
+      'source',
+      'eventtype',
+      Time.parse('2000-01-01T12:34:56+09:00'),
+      'https://url.com',
+      "summary\nwith\nlineseparators",
+      'detail\nwith\nlineseparators'
+    )
+
+    oneline = event.oneline
+
+    expect(oneline).to eq '2000/01/01 12:34 eventtype [summary with lineseparators](https://url.com)'
   end
 end
